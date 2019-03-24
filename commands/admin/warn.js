3999145724d9d6c00705fc1f9d50eedde956340d
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-03-24 07:44:02 +0700

    Create warn.js

---
+++ commands/admin/warn.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = "!";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class warnCommand extends commando.Command{
+  constructor(client) 
+  {
+    super(client, {
+      name: 'warn', 
+      group: 'network',
+      memberName: 'warn',
+      description: "Warns a user"
+    });
+  }
+  async run (message, args)
+  {
+        var warnargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        var warnUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(warnargs[0]));
+        var warnreason = warnargs.join(" ").slice(27);
+        if (!warnUser) return message.channel.send({embed: new Discord.RichEmbed()
+          .setDescription(":x: **Missing args**")
+          .setColor("#FF4040")
+          .addField("->", "!warn [User] [Reason]")});
+    
+        var supportteamerole = message.guild.roles.find(`name`, "Policeunit");
+
+        if (!supportteamerole.id) message.channel.send("Couldn't find a role called ``Policeunit``.");
+        
+        if (!message.member.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to warn others");
+
+        if (warnUser.roles.has(supportteamerole.id)) return message.channel.send("Insufficient permission. You do not have permission to warn staff members!");
+        
+        if (!warnreason) return message.channel.send({embed: new Discord.RichEmbed()
+          .setDescription(":x: **Missing args**")
+          .setColor("#FF4040")
+          .addField("->", "!warn [User] [Reason]")});
+        
+          var warnembed = new Discord.RichEmbed()
+          .setColor("#4286f4")
+          .setTitle('**Warn | Information**')
+          .setDescription(`${warnUser}, Warned by ${message.author}`)
+          .addField(`**REASON:**`, warnreason)
+          .setFooter("Moderation Bot [] Created by hieu#0843")
+
+          let logschannel = message.guild.channels.find(`name`, "logs");
+          if(!logschannel) return message.channel.send("Couldn't find a channel called ``#logs``.");
+
+          logschannel.send(warnembed);
+
+        message.channel.send(`${warnUser} has successfully been warned.`)
+
+        warnUser.sendMessage(warnUser + " Hey you have been warned for " + '[' + warnreason + ']').catch()
+
+        message.delete();
+  }
+}
+
+module.exports = warnCommand
